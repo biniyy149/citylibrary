628597e4759207ce143e28f12b25d6c06696b67f
Biniyam Yemane <biniyamyemane@yahoo.com>
2021-02-04 13:38:28 -0600

    final citlibrary project

--- js/library.js
+++ js/library.js
@@ -17,15 +17,16 @@ async function fetchBookLists(){
         let output = document.getElementById("mytable")
         for(let i = 0; i <= res.length; i++){
             let count = 1
+            
             output.innerHTML += "<td>" + +(count+i) + "</td>"
         + "<td>" + res[i].isbn + "</td>" + "<td>" + res[i].title + "</td>" +
         "<td>" + res[i].overdueFee + "</td>" + "<td>" + res[i].publisher + "</td>" +
-        "<td>" + res[i].datePublished + "</td>"
+        "<td>" + res[i].datePublished +  "<td>" + "<a href='editBook.html'>Edit</a>" + "</td>" +
+        "<td>" + "<a href = ''> Delete </a>" + "</td></tr>" + "</td>" + '</table>'
         }
     } catch (err) {console.log(err)}
 
 
+
 }
-
-
-
+ 
